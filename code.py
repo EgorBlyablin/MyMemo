c9eafcd5526315c14737d8c78569164f86db3427
Blyablin Egor Alexeevich <eg.blyablin@yandex.ru>
2020-11-12 17:55:54 +0300

    Add comments

--- code.py
+++ code.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import os
-from os import mkdir
+from os import mkdir ## import block
 import subprocess
 import sys
-
-#from PyQt5.QtGui import QFont, QFontDatabase
 
 try:
     import datetime
@@ -31,7 +28,7 @@ except:
 
 
 class MainWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self): ## initialization
         super().__init__()
         uic.loadUi("src/forms/main_window.ui", self)
         self.about_window = AboutWindow(self)
@@ -45,14 +42,14 @@ class MainWindow(QMainWindow):
             "last": self.last
         }
         
-        try:
+        try: ## open .json with notes
             with open("userFiles/data.json", "r", encoding="utf8") as file:
                 self.file = json.load(file)
                 self.notes_dict = self.file["notes_dict"]
                 self.last = self.file["last"]
                 self.redraw_list_menu()
                 self.selectItem(self.last)
-        except:
+        except: ## create .json if there is no such
             mkdir("userFiles")
             with open("userFiles/data.json", "w", encoding="utf8") as file:
                 self.file = {
@@ -79,7 +76,7 @@ class MainWindow(QMainWindow):
             )
         self.actionQuit.triggered.connect(lambda: self.close_app())
     
-    def new_note(self):
+    def new_note(self): ## creating new note
         if self.unsaved == True:
             self.about_quit.save_changes_window()
         self.last = None
@@ -93,7 +90,7 @@ class MainWindow(QMainWindow):
         self.saveButton.setDisabled(True)
         self.actionSaveNote.setDisabled(True)
     
-    def selectItem(self, item_name):
+    def selectItem(self, item_name): ## func to get note from list
         if item_name != None:
             iterator = QTreeWidgetItemIterator(self.notesList, QTreeWidgetItemIterator.All)
             while iterator.value():
@@ -103,8 +100,7 @@ class MainWindow(QMainWindow):
                     self.load_note(item, None)
                 iterator += 1
     
-    def note_changed(self): # [note] if note title changes - 
-                            # window title changes too
+    def note_changed(self): # if note title changes window title changes too
         if self.get_note_title().strip() != "":
             self.setWindowTitle("MyMemo - " + self.get_note_title().strip())
         else:
@@ -117,7 +113,7 @@ class MainWindow(QMainWindow):
             self.saveButton.setEnabled(True)
             self.actionSaveNote.setEnabled(True)
     
-    def redraw_list_menu(self):
+    def redraw_list_menu(self): ## redrawing list one by one
         self.notesList.clear()
         for element in list(self.notes_dict.keys()):
             self.last = QtWidgets.QTreeWidgetItem(self.notesList, 
@@ -125,7 +121,7 @@ class MainWindow(QMainWindow):
                                        self.notes_dict[element]["date"]]
                                       ).text(0)
         
-    def note_unsaved(self, status=True):
+    def note_unsaved(self, status=True): ## marks that note unsaved
         if status:
             if self.windowTitle()[-1] != "*":
                 self.setWindowTitle(self.windowTitle() + "*")
@@ -139,7 +135,7 @@ class MainWindow(QMainWindow):
             self.actionSaveNote.setEnabled(True)
             self.saveButton.setEnabled(True)
     
-    def load_note(self, item, last_item):
+    def load_note(self, item, last_item): ## load note from memory
         if item != last_item and item != None:
             self.last = item.text(0)
             if item != None:
@@ -157,7 +153,7 @@ class MainWindow(QMainWindow):
                 self.saveButton.setDisabled(True)
                 self.actionSaveNote.setDisabled(True)
 
-    def delete_note(self):
+    def delete_note(self): ## delete note
         self.last = None
         if len(self.notesList.selectedItems()) != 0:
             note_name = self.notesList.selectedItems()[0].text(0)
@@ -192,7 +188,7 @@ class MainWindow(QMainWindow):
     def get_note_text(self):
         return self.noteTextEdit.toPlainText() # [note] returns [' ', '\t', '\n']
     
-    def closeEvent(self, event: QtGui.QCloseEvent):
+    def closeEvent(self, event: QtGui.QCloseEvent): ## if user want to close window we save note [trigger]
         if self.unsaved == False:
             app.closeAllWindows()
             event.accept()
@@ -206,7 +202,7 @@ class MainWindow(QMainWindow):
             self.close_app()
             event.ignore()
     
-    def close_app(self):
+    def close_app(self): ## closing app from code
         with open("userFiles/data.json", "w", encoding="utf8") as file:
             self.file = {
                 "notes_dict": self.notes_dict,
@@ -221,17 +217,17 @@ class MainWindow(QMainWindow):
             app.closeAllWindows()   
 
 
-class TableWidgetItem(QtWidgets.QTableWidgetItem):
+class TableWidgetItem(QtWidgets.QTableWidgetItem): ## reorganization of sorting
     def __lt__(self, other):
         print(0)
         if self.notesList.sortColumn == 1:
             print(1)
             key1 = self.text(0)
             key2 = other.text(0)
-            return self.notesList[key1]["date_to_seconds"] < self.notesList[key2]["date_to_seconds"]
+            return self.notesList[key2]["date_to_seconds"] < self.notesList[key1]["date_to_seconds"]
         
 
-class AboutWindow(QWidget):
+class AboutWindow(QWidget): ## window can be called from upper bar or pressing CTRL+H
     def __init__(self, *args):
         super().__init__()
         uic.loadUi("src/forms/about_window.ui", self)
@@ -240,7 +236,7 @@ class AboutWindow(QWidget):
         if self.isVisible():
             self.close()
         else:
-            self.move(
+            self.move( ## middle align
                 mainWin.x() + (mainWin.width() // 2 - self.width() // 2), mainWin.y() + (mainWin.height() // 2 - self.height() // 2)
                 )
             self.show()
@@ -249,7 +245,7 @@ class AboutWindow(QWidget):
         self.close()
 
 
-class AboutQuit(QWidget):
+class AboutQuit(QWidget): ## window activating if you try to close app without saving
     def __init__(self, *args):
         super().__init__()
         uic.loadUi("src/forms/about_quit.ui", self)
@@ -257,7 +253,7 @@ class AboutQuit(QWidget):
         self.deleteButton.clicked.connect(lambda: self.delete_changes())
         self.cancelButton.clicked.connect(lambda: self.cancel_changes())
 
-    def save_changes_window(self): # [note] if note isn't saved - ask to save
+    def save_changes_window(self): ## if note isn't saved - ask to save
         self.move(
             mainWin.x() + (mainWin.width() // 2 - self.width() // 2),
             mainWin.y() + (mainWin.height() // 2 - self.height() // 2)
@@ -314,13 +310,13 @@ class AboutQuit(QWidget):
         mainWin.deleteButton.setEnabled(True)
         mainWin.actionRemoveNote.setEnabled(True)
         
-        with open("userFiles/data.json", "w", encoding="utf8") as file:
+        with open("userFiles/data.json", "w", encoding="utf8") as file: ## writing to file
             mainWin.file = {
                 "notes_dict": mainWin.notes_dict,
                 "last": mainWin.last
             }
             json.dump(mainWin.file, file, indent=4)
-        if self.isVisible():
+        if self.isVisible(): ## app closes only if it asked about quit, else just saving without exiting
             mainWin.close_app()
     
     def delete_changes(self):
@@ -345,7 +341,7 @@ def except_hook(cls, exception, traceback):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    # QFontDatabase.addApplicationFont("src/fonts/obelix_pro.ttf")
+    ## QFontDatabase.addApplicationFont("src/fonts/obelix_pro.ttf")
     mainWin = MainWindow()
     mainWin.show()
     sys.excepthook = except_hook
