2999416bd3a63835d5a4c6c52320835f5405177a
Blyablin Egor Alexeevich <eg.blyablin@yandex.ru>
2020-11-15 13:36:25 +0300

    Добавление презентации

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-        mkdir("userFiles")
-        with open("userFiles/data.json", "w", encoding="utf8") as file:
-            self.file = {
-                "notes_dict": self.notes_dict,
-                "last": self.last
-            }
-            json.dump(self.file, file, indent=4)
-            self.redraw_list_menu()
